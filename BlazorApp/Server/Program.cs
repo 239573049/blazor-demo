adc6b60600b7a6cb1529911d97f5784bdc293bb7
239573049@qq.com <239573049@qq.com>
2022-01-30 08:51:02 +0800

    修改

--- BlazorApp/Server/Program.cs
+++ BlazorApp/Server/Program.cs
@@ -1,4 +1,7 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.ResponseCompression;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +10,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());//覆盖用于创建服务提供者的工厂
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>//依赖注入
+{
+    var basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+    var servicesDllFile = Path.Combine(basePath, "Service.dll");//需要依赖注入的项目生成的dll文件名称
+    var repositoryDllFile = Path.Combine(basePath, "Repository.dll");
+    var mongoDBRepositoryDllFile = Path.Combine(basePath, "MongoDBRepository.dll");
+    var assemblysServices = Assembly.LoadFrom(servicesDllFile);
+    containerBuilder.RegisterAssemblyTypes(assemblysServices)
+        .Where(x => x.FullName != null && x.FullName.EndsWith("Service"))//对比名称最后是否相同然后注入
+              .AsImplementedInterfaces()
+              .InstancePerDependency();
+    var assemblysRepository = Assembly.LoadFrom(repositoryDllFile);
+    containerBuilder.RegisterAssemblyTypes(assemblysRepository)
+        .Where(x => x.FullName != null && x.FullName.EndsWith("Repository"))
+              .AsImplementedInterfaces()
+              .InstancePerDependency();
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

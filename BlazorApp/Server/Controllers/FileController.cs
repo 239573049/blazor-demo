ad7bbe6fbd3205da43bce4b268de7e486395f290
239573049@qq.com <239573049@qq.com>
2022-01-31 19:18:24 +0800

    增加接口

--- BlazorApp/Server/Controllers/FileController.cs
+++ BlazorApp/Server/Controllers/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorApp.Server.Controllers
@@ -7,10 +8,17 @@ namespace BlazorApp.Server.Controllers
     [ApiController]
     public class FileController : ControllerBase
     {
+        private readonly IFileService _fileService;
         public FileController(
+            IFileService fileServic
             )
         {
-
+            _fileService = fileServic;
+        }
+        [HttpGet]
+        public IActionResult IsExit(string filePath)
+        {
+            return new OkObjectResult(_fileService.IsExitFile(filePath));
         }
     }
 }

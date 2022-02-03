2c3b2f99e0e0e11485384aafdb30b10284c33182
239573049@qq.com <239573049@qq.com>
2022-02-03 22:44:41 +0800

    修改

--- BlazorApp/Client/obj/Debug/net6.0/service-worker/service-worker.js
+++ BlazorApp/Client/obj/Debug/net6.0/service-worker/service-worker.js
@@ -2,4 +2,4 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
-/* Manifest version: WkLEtnzy */
+/* Manifest version: p5B04XSM */

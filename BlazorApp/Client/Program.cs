7b7e8bea51357a08d1db7b5dbd3b1a57f500f798
239573049@qq.com <239573049@qq.com>
2022-02-11 18:08:37 +0800

    增加全局处理

--- BlazorApp/Client/Program.cs
+++ BlazorApp/Client/Program.cs
@@ -10,7 +10,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddAntDesign();
 builder.Services.AddSingleton(typeof(MessageService));
-builder.Services.AddHttpClientHelper(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClientHelper(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }, a =>
+{
+    Console.WriteLine($"{a.RequestMessage?.RequestUri}:"+a.StatusCode);
+});
 var basePath = AppDomain.CurrentDomain.BaseDirectory;
 builder.Services.AddInjectSingleton(Path.Combine(basePath, "BlazorApp.Client.dll"), a => a.Name.EndsWith("Api"));
 await builder.Build().RunAsync();
